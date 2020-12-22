aefdefda818d3b94aa206873139acee6a42a8f02
Bar Spielberg <barspielberg@gmail.com>
2020-12-22 12:05:13 +0200

    IBook

---
+++ LibraryServer/LibraryCommon/Models/IBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCommon.Models
+{
+    public interface IBook
+    {
+        Guid Id { get; }
+        string Title { get; }
+        string Author { get; }
+        DateTime PublishDate { get; }
+    }
+}
