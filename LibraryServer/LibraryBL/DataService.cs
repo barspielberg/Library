7727fd4520637c30eea96a6f9aad852c82d1659b
Bar Spielberg <barspielberg@gmail.com>
2020-12-22 12:46:57 +0200

    bl

---
+++ LibraryServer/LibraryBL/DataService.cs
@@ -0,0 +1,11 @@
+﻿using LibraryCommon.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryBL
+{
+    public class DataService :IDataService
+    {
+    }
+}
