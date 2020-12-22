5464bf07d7bdf89a61da5f3689d8026013fc5e9d
Bar Spielberg <barspielberg@gmail.com>
2020-12-22 12:37:13 +0200

    Dal

---
+++ LibraryServer/LibraryDAL/LibraryContext.cs
@@ -0,0 +1,22 @@
+﻿using LibraryCommon.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDAL
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Magazine> Magazines { get; set; }
+        public DbSet<Novel> Novels { get; set; }
+        public DbSet<StudyBook> StudyBooks { get; set; }
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
