a068d3d1a3cd9dcd453f958d9225dc4a90016680
Bar Spielberg <barspielberg@gmail.com>
2020-12-23 22:55:44 +0200

    book type enum

---
+++ LibraryServer/LibraryCommon/Enums/BookType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCommon.Enums
+{
+    public enum BookType
+    {
+        Magazine, Novel,Study
+    }
+}
