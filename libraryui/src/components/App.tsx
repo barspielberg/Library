dc4c032b8688b5fec1d373d9d0dfebe7030d40a4
Bar Spielberg <barspielberg@gmail.com>
2020-12-26 18:49:41 +0200

    nav bar bottom

--- libraryui/src/components/App.tsx
+++ libraryui/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { createMuiTheme, makeStyles, ThemeProvider } from "@material-ui/core";
 import { blue, green } from "@material-ui/core/colors";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -18,24 +18,38 @@ const theme = createMuiTheme({
   },
 });
 
+const useStyles = makeStyles({
+  navbar: {
+    position: "fixed",
+    bottom: "0",
+    width: "100%",
+    zIndex: 100,
+  },
+  footer: {
+    height: "4rem",
+  },
+});
+
 const App: React.FC = () => {
+  const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <div className="App">
-          <header>
-            <Navbar />
-          </header>
+        <div>
           <section>
             <Switch>
               <Route path="/manager" component={ManagerPage} />
               <Route path="/" component={HomePage} />
             </Switch>
           </section>
+          <nav className={classes.navbar}>
+            <Navbar />
+          </nav>
+          <footer className={classes.footer} />
         </div>
       </BrowserRouter>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
